5241242579d10b49e4512ce0f5d6c66c954226b8
will <wmorales0207@gmail.com>
2022-08-26 15:11:16 +0200

    leyendo de un Json Local

---
+++ lib/src/providers/provider.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/services.dart' show rootBundle;
+// e
+
+class MenuProvider {
+  List<dynamic> opciones = [];
+  MenuProvider() {
+    _cargarData();
+  }
+
+  _cargarData() {}
+}
