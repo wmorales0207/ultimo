fd69ad8eefb2cd5aef605d1928e403ee3b5ec4bd
will <wmorales0207@gmail.com>
2022-08-26 16:28:08 +0200

    creando una funcion future

--- lib/src/providers/provider.dart
+++ lib/src/providers/provider.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/services.dart' show rootBundle;
 // en esta importacion se se aclara que solo se desea el rootBundle*( que es necesario para trabajar con JSON) que una parte de todo lo
 // lo que compone el package:flutter/services.dart.
@@ -5,15 +7,18 @@ import 'package:flutter/services.dart' show rootBundle;
 class _MenuProvider {
   List<dynamic> opciones = [];
   _MenuProvider() {
-    _cargarData();
+    //_cargarData();
   }
 
-  _cargarData() {
-    rootBundle.load('data/menu_opts.json').then((value) {
-      print(value);
-    });
+//Esta funcion devuelve un future que contiene una lista... de esta forma es que se puede
+// garantizar que lo que se devuelva sea correcto.
+  Future<List<dynamic>> _cargarData() async {
+    var then = rootBundle.load('data/menu_opts.json');
+    Map dataMap = json.decode(then.toString());
+    opciones = dataMap['rutas'];
+    return opciones;
   }
 }
 
 final menuProvider =
-    _MenuProvider();// de esta manera solo se expone la instncia de _Menu provider
+    _MenuProvider(); // de esta manera solo se expone la instncia de _Menu provider
