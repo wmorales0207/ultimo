92941ef14f10509c4b3427a03450d0a684271dca
will <wmorales0207@gmail.com>
2022-08-26 10:45:16 +0200

    creados children estaticos

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:ultimo/src/pages/home_temp.dart';
 
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        title: 'Componentes App',
+        debugShowCheckedModeBanner: false,
+        home: HomePageTemp());
+  }
+}
