c050d5f8b5770440620c1e71b9b3730a64cf2625
will <wmorales0207@gmail.com>
2022-08-29 18:22:45 +0200

    Creando Sist Archiv 5-video5

--- lib/src/pages/app.dart
+++ lib/src/pages/app.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+// Notas importantes.
+/*1 Despues de cada asigancion de propiedad a un widget va una coma
+2 Aunque en las asignaciones hallan widget dentro de widget se pone coma igual
+3 Para las cadenas se puede usar lo mismo "", ''
+4 todas las contrucciones de wiget son objetos que llevan un NEW, puedes ponerselo o no pero es un new.
+5 StatelessWidget y StateFull son Widgetes obstractos, siempre se usan para pintar componentes, claro en dependencia del comporatmiento que se quiera lograr se una uno u otro.
+ */
 
 class MyExampleApp extends StatelessWidget {
   const MyExampleApp({super.key});
